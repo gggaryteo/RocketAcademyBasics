2f8c184149fbc8f67d4963397b7687156f6d6404
gggaryteo <101798200+gggaryteo@users.noreply.github.com>
2022-04-04 20:14:48 +0800

    Updated SPS Part 1 & 2 SPS Part 1 and 2. Some changes to Day 3 and 4.

--- day04-loops/pre-class/script.js
+++ day04-loops/pre-class/script.js
@@ -1,23 +1,32 @@
 var followAlongMain = function (input) {
   // Attempt the Follow Along exercise from the Loops module below with followAlongMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
 
 var simpleLoopMain = function (input) {
+  var myOutputValue = "";
+  var counter = 0;
+  while (counter < input) {
+    if (counter <= 6) {
+      myOutputValue += "hello";
+    }
+    counter += 1;
+  }
   // Attempt the Simple Loop with Variations exercise from the Loops module below with simpleLoopMain as the main function.
-  var myOutputValue = 'hello world';
   return myOutputValue;
 };
 
-var loopWithinLoopMain = function (input) {
-  // Attempt the Loop Within Loop exercise from the Loops module below with loopWithinLoopMain as the main function.
-  var myOutputValue = 'hello world';
-  return myOutputValue;
-};
+/* 
+Create nested loops in the loopWithinLoopMain function, where the outer loop runs 3 times and the inner loop runs 3 times per outer loop. 
+Concatenate "hello" to myOutputValue in the inner loop. How many times do we see "hello"?
+*/
+
+var loopWithinLoopMain = function (input) {};
+// Attempt the Loop Within Loop exercise from the Loops module below with loopWithinLoopMain as the main function.
 
 var infiniteLoopMain = function (input) {
   // Attempt the Infinite Loop exercise from the Loops module below with infiniteLoopMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
--- project1-sps/script.js
+++ project1-sps/script.js
@@ -4,66 +4,98 @@ the program internally randomly chooses scissors, paper, or stone, and the progr
 Rules: scissors beats paper, paper beats stone, and stone beats scissors. If both parties choose the same object, it's a draw.
 */
 
-// User input scissors, paper, or stone.
-// Computer outputs scissors, paper, or stone at random.
+// State global variables
+var currentGameMode = "waiting For Username";
+var userName = "";
+var numUserWins = 0;
+var numComputerWins = 0;
+var totalGamesPlayed = 0;
+var winPercentage = (numUserWins / totalGamesPlayed) * 100;
+var losePercentage = (numComputerWins / totalGamesPlayed) * 100;
+
 // Assign scissors, paper, stone to a value respectively.
 // Randomize the output given by the computer by creating a helper function with Math.random
-
-var generateRandomInteger = function (max) {
-  var randomInteger = Math.floor(Math.random() * max + 1);
-  return randomInteger;
+// Computer outputs scissors, paper, or stone at random.
+var generateRandomInteger = function () {
+  return Math.floor(Math.random() * 3 + 1);
 };
 
-var generatePssRandom = function () {
-  var numberOfSigns = 3;
-  var randomSignByComputer = generateRandomInteger(numberOfSigns);
+var generatePaperScissorsStone = function () {
+  var randomSignByComputer = generateRandomInteger();
   console.log(`Computer generated sign is ${randomSignByComputer}`);
   if (randomSignByComputer == 1) {
     return `scissors`;
   }
   if (randomSignByComputer == 2) {
     return `paper`;
-  }
-  if (randomSignByComputer == 3) {
+  } else {
     return `stone`;
   }
 };
 
-var main = function (signByHuman, computerGeneratedPss) {
-  // Predefine Function
-  var signByHuman = signByHuman.toLowerCase();
+// Create a Username function
+var initialUsername = function (inputName) {
+  userName = inputName.charAt(0).toUpperCase() + inputName.slice(1);
+  currentGameMode = "Play Scissors, Paper, Stone.";
+  return `Hello, ${userName}. Please enter scissors ✂️, paper 📄, or stone 🗿 to start playing.`;
+};
+
+// Refactoring Code: Scissors Paper Stone Game Logic
+var playPaperScissorsStone = function (userName, userGuess) {
+  var userGuess = userGuess.toLowerCase();
 
   // User input Validation
-  if (
-    signByHuman != `scissors` &&
-    signByHuman != `paper` &&
-    signByHuman != `stone`
-  ) {
-    return `Please enter scissors, paper, or stone only.`;
+  if (userGuess != `scissors` && userGuess != `paper` && userGuess != `stone`) {
+    return `Hello ${userName}! This is an invalid input. Please enter scissors ✂️, paper 📄, or stone 🗿 only.`;
   }
+
   // Computer generated Scissors Paper Stone
-  var computerGeneratedPss = generatePssRandom();
-  console.log(`Computer generated sign is ${computerGeneratedPss}`);
-  var genericOutput = `You have chosen ${signByHuman}. Bot Lewis picked ${computerGeneratedPss}.`;
+  var computerObject = generatePaperScissorsStone();
+  console.log(`Computer generated sign is ${computerObject}`);
+  var genericOutput = `Hi ${userName}, you have chosen ${userGuess}. <br> Bot Lewis picked ${computerObject}.`;
 
   // Human wins Bot Computer Scenario
   if (
-    signByHuman == "scissors" &&
-    computerGeneratedPss == "paper" &&
-    signByHuman == "paper" &&
-    computerGeneratedPss == "stone" &&
-    signByHuman == "stone" &&
-    computerGeneratedPss == "scissors"
+    (userGuess == `scissors` && computerObject == `paper`) ||
+    (userGuess == `paper` && computerObject == `stone`) ||
+    (userGuess == `stone` && computerObject == `scissors`)
   ) {
-    genericOutput += ` You have won!`;
+    numUserWins++;
+    console.log(`The number of times user won: ${numUserWins}`);
+    genericOutput += `<br><br> You emerged victorious! <br> <br>`;
   }
   // Human Draws Against Bot Computer Scenario
-  else if (signByHuman == computerGeneratedPss) {
-    genericOutput += ` This match ends in a draw!`;
+  else if (userGuess == computerObject) {
+    numUserWins;
+    numComputerWins;
+    genericOutput += `<br><br> This match ends in a draw! <br><br>`;
   }
   // Human Loses Against Bot Computer Default Output
   else {
-    genericOutput += ` You have lost! Try again next time...`;
+    numComputerWins++;
+    console.log(`The number of times Bot Lewis won: ${numComputerWins}`);
+    genericOutput += `<br><br> Don't lose hope... Try again next time. <br> <br>`;
   }
-  return genericOutput;
+  totalGamesPlayed++;
+  console.log(`Total Games: ${totalGamesPlayed}`);
+  winPercentage = (numUserWins / totalGamesPlayed) * 100;
+  losePercentage = (numComputerWins / totalGamesPlayed) * 100;
+
+  var winLossInfo = `
+  Total Games Played: ${totalGamesPlayed} 
+  <br><br> Wins: ${numUserWins} (${winPercentage.toFixed(
+    2
+  )}%) <br> <br> Loss: ${numComputerWins} (${losePercentage.toFixed(2)}%)`;
+
+  return genericOutput + winLossInfo;
+};
+
+var main = function (input) {
+  // Set to name mode first
+  if (currentGameMode == "waiting For Username") {
+    return initialUsername(input);
+    // Set to Scissors, Paper, Stone Mode
+  } else if ((currentGameMode = "Play Scissors, Paper, Stone.")) {
+    return playPaperScissorsStone(userName, input);
+  }
 };
